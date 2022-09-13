72a966d38af91b9e378971e758282bfe572473b2
Matthias Goupil <matthiasrobin.goupil@montreal.ca>
2022-09-12 22:19:43 -0400

    fix: add parsing error handling

--- src/epg/client.ts
+++ src/epg/client.ts
@@ -2,6 +2,7 @@ import { createWriteStream, promises as fsPromises } from 'fs';
 import * as superagent from 'superagent';
 
 import { config } from '../configuration';
+import { logger } from '../logger';
 import { IEpg } from './definitions';
 import { parser } from './parser';
 
@@ -9,7 +10,7 @@ export const client = {
   fetch: async (): Promise<IEpg> => {
     const { host, port, username, password } = config.api;
 
-    return new Promise<IEpg>(async (resolve) => {
+    return new Promise<IEpg>(async (resolve, reject) => {
       const fileName = `${config.directories.epg.temp}/response_${Date.now()}.xml`;
 
       superagent
@@ -19,7 +20,13 @@ export const client = {
         .pipe(createWriteStream(fileName))
         .on('close', () => {
           fsPromises.readFile(fileName).then((content) => {
-            resolve(parser.parse(content));
+            try {
+              const datas = parser.parse(content);
+              resolve(datas);
+            } catch (err) {
+              logger.error('Error while parsing http response', err);
+              reject(err);
+            }
           });
         });
     });
